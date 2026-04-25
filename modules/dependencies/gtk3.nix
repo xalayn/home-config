00dd83eddfe77331c129535adba182a36c6e4309
Alex Kumpula <alex.kumpula01@gmail.com>
2026-04-25 17:35:55 -0600

    Fix

--- modules/dependencies/gtk3.nix
+++ modules/dependencies/gtk3.nix
@@ -2,15 +2,30 @@
 {
   gtk = {
     enable = true;
-    colorScheme = "dark";
+    
+    # Specify a theme that supports dark mode
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
 
   home.packages = with pkgs; [
     gsettings-desktop-schemas
   ];
+
+  # This helps ensure apps respect the dark preference via dconf
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 }
-
-
-
-
-# home-manager/option/gtk.gtk3.enable
